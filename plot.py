f1c3fc2b71e88a76a9a55ca9aa97118ab62da418
giammisimo <gianmarco.simonazzi@studenti.unipr.it>
2024-11-27 14:29:45 +0100

    Changed name to plot scripts. Added grid.txt to gitignore

--- plot.py
+++ plot.py
@@ -1,105 +1,104 @@
-import tkinter as tk
+"""
+Shows solution from Gecode.
+Takes a file path as input.
+To launch
+python3 plot.py <solution>
+"""
 import sys
+import matplotlib.pyplot as plt
+import numpy as np
 
-solution = open(sys.argv[1],'r').read()
+def read_solution(filename):
+    """
+    Read and parse the solution file
+    """
+    with open(filename, 'r') as f:
+        solution = f.read()
+    
+    # Parse parameters
+    parameters = solution.split('***')[0].split('\n')[:-1]
+    x, y, k, n = tuple([int(par.split(' ')[1]) for par in parameters])
+    
+    # Parse boxes
+    boxes = int(solution.split('***')[3].split('\n')[1].split(' ')[1])
+    
+    # Parse positions and sizes
+    positions = solution.split('***')[1][len('positions:'):].split('\n')[1:-1]
+    positions = [(int(pos.split(',')[0]),int(pos.split(',')[1])) for pos in positions[:boxes]]
+    
+    sizes = solution.split('***')[2][len('sizes:'):].split('\n')[1:-1]
+    sizes = [(int(size.split(',')[0]),int(size.split(',')[1])) for size in sizes[:boxes]]
+    
+    return x, y, k, n, boxes, positions, sizes
 
-parameters = solution.split('***')[0].split('\n')[:-1]
-x, y, k, n = tuple([int(par.split(' ')[1]) for par in parameters])
-print('x',x)
-print('y',y)
-print('k',k)
-print('n',n)
+def visualize_grid(x, y, k, boxes, positions, sizes):
+    """
+    Visualize the grid using Matplotlib
+    """
+    # Predefined color palette
+    colors = [
+        'yellow', 'red', 'green', 'blue', 'orange', 
+        'purple', 'brown', 'pink', 'cyan', 'magenta'
+    ]
+    
+    # Create a new figure and axis
+    fig, ax = plt.subplots(figsize=(10, 10))
+    
+    # Draw the grid
+    for i in range(k):
+        for j in range(k):
+            ax.add_patch(plt.Rectangle((j, k-1-i), 1, 1, 
+                                       fill=False, 
+                                       edgecolor='black', 
+                                       linewidth=1))
+    
+    # Place boxes
+    for idx in range(boxes):
+        pos = positions[idx]
+        size = sizes[idx]
+        color = colors[idx % len(colors)]
+        # Adjust for coordinate system (bottom-left origin)
+        ax.add_patch(plt.Rectangle((pos[0]-1, pos[1]-1), 
+                                   size[0], size[1], 
+                                   color=color, 
+                                   alpha=0.7, 
+                                   edgecolor='black'))
+    
+    # Set plot properties
+    ax.set_xlim(0, k)
+    ax.set_ylim(0, k)
+    ax.set_aspect('equal')
+    ax.set_title(f'k={k} / x = {x} / y = {y} / boxes = {boxes}')
+    ax.set_xlabel('X coordinate')
+    ax.set_ylabel('Y coordinate')
+    
+    # Add grid labels
+    plt.xticks(np.arange(0.5, k, 1), range(1, k+1))
+    plt.yticks(np.arange(0.5, k, 1), range(1, k+1))
+    
+    return fig, ax
 
-boxes = int(solution.split('***')[3].split('\n')[1].split(' ')[1])
-print('boxes',boxes)
+def main():
+    # Check if filename is provided
+    if len(sys.argv) < 2:
+        print("Please provide a solution file as an argument.")
+        sys.exit(1)
+    
+    # Read solution
+    x, y, k, n, boxes, positions, sizes = read_solution(sys.argv[1])
+    print(k,x,y)
+    print(n,boxes)
+    print(positions)
+    print(sizes)
+    
+    # Create visualization
+    fig, ax = visualize_grid(x, y, k, boxes, positions, sizes)
+    
+    # Save the figure
+    #plt.savefig('grid_solution.png', dpi=300, bbox_inches='tight')
+    
+    # Display the figure
+    plt.show()
 
-positions = solution.split('***')[1][len('positions:'):].split('\n')[1:-1]
-positions = [(int(pos.split(',')[0]),int(pos.split(',')[1])) for pos in positions[:boxes]]
-
-sizes = solution.split('***')[2][len('sizes:'):].split('\n')[1:-1]
-sizes = [(int(size.split(',')[0]),int(size.split(',')[1])) for size in sizes[:boxes]]
-
-print('positions',positions)
-print('sizes',sizes)
-
-# old parsing for the default minzinc formatting
-# positions = 
-# [| 1,  1
-#  | 1, 11
-#  | 6,  6
-#  | 6,  1
-#  |];
-# sizes = 
-# [|  5, 10
-#  | 10,  5
-#  | 10,  5
-#  | 10,  5
-#  |];
-# s = re.sub('\n','',s)
-# s = re.sub('\[\| ','[[',s)
-# s = re.sub(' \|\]',']]',s)
-# s = re.sub(' \| ','],[',s)
-# s = re.sub('[a-z]+ = ','',s)
-# positions, sizes = (*s.split(';')[:-1],)
-# positions = eval(positions)
-# sizes = eval(sizes)
-
-class GridGUI:
-    def __init__(self, grid_size: int, cell_size: int):
-        # Creating the root window
-        self.root = tk.Tk()
-        self.root.title("Grid GUI")
-
-        # Creating the canvas widget
-        self.canvas = tk.Canvas(self.root, width=grid_size*cell_size, height=grid_size*cell_size)
-        self.canvas.pack()
-
-        # Setting the grid size and cell size
-        self.grid_size = grid_size
-        self.cell_size = cell_size
-
-        # Dictionary to store the colors for different square sizes
-        self.square_colors = {
-            1: "yellow",
-            2: "red",
-            3: "green",
-            4: "blue",
-            5:"orange",
-            6:"purple",
-            7: "brown"
-        }
-
-    def draw_grid(self):
-        """
-        Draws the grid on the canvas.
-        """
-
-        for i in range(self.grid_size):
-            for j in range(self.grid_size):
-                x1 = j * self.cell_size
-                y1 = i * self.cell_size
-                x2 = x1 + self.cell_size
-                y2 = y1 + self.cell_size
-                self.canvas.create_rectangle(x1, y1, x2, y2, outline="black")
-
-    def place_box(self, index: int, sizex: int, sizey: int, col: int, row: int):
-        color = self.square_colors[index % len(self.square_colors) + 1]
-
-        x1 = col * self.cell_size
-        y1 = row * self.cell_size
-        x2 = x1 + sizex * self.cell_size
-        y2 = y1 + sizey * self.cell_size
-
-        self.canvas.create_rectangle(x1, y1, x2, y2, fill=color)
-
-    def run(self):
-        self.root.mainloop()
-
-## Gridsize(k, cellSize)
-gui = GridGUI(k, 20)
-gui.draw_grid()
-
-for i in range(boxes):
-    gui.place_box(i,sizes[i][0],sizes[i][1],positions[i][0]-1,positions[i][1]-1)
-
-gui.run()
+if __name__ == "__main__":
+    main()
