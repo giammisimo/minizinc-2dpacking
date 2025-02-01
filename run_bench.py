941fc83760bc0dd9f6556eaf64280811cd458ed8
giammisimo <gianmarco.simonazzi@studenti.unipr.it>
2025-02-01 10:23:06 +0100

    Rewrote run_bench

--- run_bench.py
+++ run_bench.py
@@ -1,78 +1,83 @@
-import os
-import sys
-import platform
+import argparse
+import subprocess
+import time
 
-number_of_benchmarks = 36
-MZN_FILE = sys.argv[1]
-
-# MacOS or Linux
-def check_operating_system():
-    os_name = platform.system()
-    if os_name == 'Darwin':
-        return 'macos'
-    elif os_name == 'Linux':
-        return 'linux'
-    else:
-        return 'unsupported'
+TIMER_INT = 0.1
+TIME_LIMIT = 300000
+SOLVER = 'chuffed'
+OUT_DIR = './benchmarks-results'
     
-def read_mzn_file(mzn_file_path):
-    # Works with benchmarks with the following pattern
-    # k = [num1]; x = [num2];
-    vars = {'k':None, 'x':None, 'y':None}
-    with open(mzn_file_path) as f:
-        s = f.read()
-        exec(s, None, vars)
-    return vars['k'], vars['x'], vars['y']
-
-def can_continue(counter: int) -> bool:
-    return counter < number_of_benchmarks
-
-
-def main():
-    benchmark_folder = './benchmarks'
-    results_folder = './benchmarks-results'
-
-    # Lista dei file nella directory
-    files = os.listdir(benchmark_folder)
-
-    # Ordinare i file
-    ordered_benchmarks_files = sorted(files)
-    
+def main(mzn_model: str, solver: str, time_limit: int, out_dir: str):
+    X = [5, 10, 15, 20]
+    K = [a - 1 for a in range(10, 101, 10)]
     counter = 0
 
-    for file_name in ordered_benchmarks_files:  
-        if not file_name.endswith(('.dzn','.mzn')):
-            continue
+    for k in K:
+        # This variable is used to track time outs for the same pallet
+        # Once a box size times out, all smaller sizes will time out as well
+        # (the problem gets harder)
+        timed_out = False
 
-        counter += 1
+        for x in X[::-1]:
+            counter += 1
+            if timed_out:
+                print(f'Test {counter} skipped - pallet timed out')
+                continue
+            y = x + 5
+            print(f'Test {counter}: k = {k}, x = {x}, y = {y}') 
 
-        if not can_continue(counter):
-            break
-        
-        mzn_file_path = os.path.join(benchmark_folder, file_name)
+            file_name = f'bench{k}-{x}'
 
-        k, x, y = read_mzn_file(mzn_file_path)
-
-        print(f'Running {mzn_file_path} with k = {k}, x = {x}, y = {y}')      
-
-        file_name_without_extension = file_name.split('.')[0]
-        bench_path = f"{benchmark_folder}/{file_name}"
-        
-        if check_operating_system() == 'macos':
-            os.system(f"minizinc --solver Gecode ./{MZN_FILE} {bench_path} --time-limit 300000 | tee ./benchmarks-results/{file_name_without_extension}.txt")
-        elif check_operating_system() == 'linux':
+            command = f"""minizinc --solver {solver} {mzn_model}  \
+--cmdline-data "x = {x}" --cmdline-data "y = {y}" --cmdline-data "k = {k}" \
+--output-time --time-limit {time_limit} > \
+{out_dir}/{file_name}.txt"""
             
-            if MZN_IDE_DIR := os.getenv('MZN'):
-                MINIZINC = os.path.join(MZN_IDE_DIR, 'bin', 'minizinc')
-            else:
-                MINIZINC = 'minizinc'
+            #print(command)
 
-            os.system(f"{MINIZINC} --output-time --solver Gecode ./{MZN_FILE} {bench_path} --time-limit 300000 | tee ./benchmarks-results/{file_name_without_extension}.txt")
-        else:
-            print("Sistema operativo non supportato.")
-            return
+            start_time = time.time()
+            process = subprocess.Popen(command, stderr=subprocess.DEVNULL, shell=True)
+
+            while process.poll() is None:
+                elapsed_time = time.time() - start_time
+                print(f"[time] {elapsed_time:.2f} s", end='\r')
+                time.sleep(TIMER_INT)
+            process.wait()
+            if elapsed_time > (time_limit / 1000):
+                timed_out = True
+
+            with open(f'{out_dir}/{file_name}.txt','r') as file:
+                print(file.read() + '\n\n')
         
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Run benchmarks for 2D Packing Minizinc model"
+    )
+    parser.add_argument(
+        "model",
+        metavar="<model>.mzn",
+        type=str, 
+        help="Path to the minizinc model to test (.mzn file)"
+    )
+    parser.add_argument(
+        "-s", "--solver", 
+        type=str, 
+        default=SOLVER, 
+        help=f"Model solver (default: {SOLVER})"
+    )
+    parser.add_argument(
+        "-t", "--time-limit", 
+        type=int, 
+        default=TIME_LIMIT, 
+        help=f"Single test Time limit in milliseconds (default: {TIME_LIMIT})"
+    )
+    parser.add_argument(
+        "-d", "--out-dir", 
+        type=str, 
+        default=OUT_DIR, 
+        help=f"Output directory for results (default: {OUT_DIR})"
+    )
+    args = parser.parse_args()
+    main(args.model, args.solver, args.time_limit, args.out_dir)
